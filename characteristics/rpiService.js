a1494e94b3dd8bd7b98222dba5e71d474831a807
Zakaria Choukchou-braham <zakaria.choukchou-braham@qiis.fr>
2018-06-22 14:42:07 +0200

    characteristic

--- characteristics/rpiService.js
+++ characteristics/rpiService.js
@@ -3,16 +3,19 @@ var bleno = require('bleno');
 var BlenoPrimaryService = bleno.PrimaryService;
 
 var WifiCharacteristic = require('./installation/wifiCharacteristic');
-var ConfigurationCharacterstic = require('./installation/configurationCharacteristic');
-var PlayerCharacteristic = require('./maintaining/playerCharacterstic');
+var ConfigurationCharacteristic = require('./installation/configurationCharacteristic');
+var PlayerCharacteristic = require('./maintaining/playerCharacteristic');
+var wifiCharacteristic = new WifiCharacteristic();
+var configurationCharacteristic = new ConfigurationCharacteristic();
+var playerCharacteristic = new PlayerCharacteristic();
 class RPIService extends BlenoPrimaryService {
     constructor(){
         super({
             uuid: '00010010-89BD-43C8-9231-40F6E305F96D',
             characterstics: [
-                new WifiCharacteristic(),
-                new ConfigurationCharacterstic(),
-                new PlayerCharacteristic()
+               wifiCharacteristic,
+               configurationCharacteristic,
+               playerCharacteristic
             ]
         });
     }
