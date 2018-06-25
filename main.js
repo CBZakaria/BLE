174641bfc8eab9b12eeb7e76e3f5f08688803da8
Zakaria Choukchou-braham <zakaria.choukchou-braham@qiis.fr>
2018-06-25 10:49:36 +0200

    qsd

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ bleno.on('advertisingStart',function(err) {
 
         bleno.setServices([service], function(err){
             console.log('setServices: ' + (err ? 'error' + err : 'success'));
-            console.log(service.characteristics.uuid);
+            console.log(service.uuid);
         });
     }
 });
